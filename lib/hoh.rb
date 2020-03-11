c51bf2ac4f74191a8ec57ffdb282afa8d3da304e
Blake <blakewoodruff4@gmail.com>
2020-03-11 18:12:21 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -51,11 +51,20 @@ def naming_system
         :sub_category => {
           :label => "Order",
           :sub_category => {
-            
+            :label => "Family",
+            :sub_category => {
+              :label => "Genus"
+              :sub_category => {
+                :label => "Species",
+                :sub_category => nil
+              }
+            }
           }
         }
       }
     }
   }
+  
+  return life
 
 end
